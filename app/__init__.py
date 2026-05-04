48f364e53a5bb1b4e8d30bb49f211f76165e8658
Алина <renatovnaalina@gmail.com>
2026-05-04 23:31:46 +0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -10,23 +10,22 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'alina_228_lol_kek_bebebe'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 app.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'static', 'avatars')
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
-
 app.config['REVIEW_PHOTO_FOLDER'] = os.path.join(basedir, 'static', 'review_photos')
 
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 os.makedirs(app.config['REVIEW_PHOTO_FOLDER'], exist_ok=True)
 
+db_session.global_init("blogs.db")
+auth(app)
+init_profile_routes(app)
+init_main_routes(app)
+init_posts_routes(app)
+
 
 def main():
-    db_session.global_init("blogs.db")
-    auth(app)
-    init_profile_routes(app)
-    init_main_routes(app)
-    init_posts_routes(app)
     app.run(port=8080, host="127.0.0.1", debug=True)
 
 
--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,8 @@
 import sys
 import os
 
-path = '/home/hikypok/myproject'
-if path not in sys.path:
-    sys.path.append(path)
+sys.path.append('/home/hikypok/myproject')
 
 from app import app
-
-from app import db_session
-db_session.global_init("blogs.db")
-
-from app.pages.autorezation import auth
-from app.pages.profile import init_profile_routes
-from app.pages.main import init_main_routes
-from app.pages.posts import init_posts_routes
-
-auth(app)
-init_profile_routes(app)
-init_main_routes(app)
-init_posts_routes(app)
 
 application = app
