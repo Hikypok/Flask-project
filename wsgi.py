6f13b45978b58f506cc548386148b6908fac8ae1
Алина <renatovnaalina@gmail.com>
2026-05-07 13:52:45 +0300

    .

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,23 @@
 import sys
 import os
 
-sys.path.append('/home/hikypok/myproject')
+path = '/home/hikypok/myproject'
+if path not in sys.path:
+    sys.path.append(path)
 
 from app import app
+
+from app import db_session
+db_session.global_init("blogs.db")
+
+from app.pages.autorezation import auth
+from app.pages.profile import init_profile_routes
+from app.pages.main import init_main_routes
+from app.pages.posts import init_posts_routes
+
+auth(app)
+init_profile_routes(app)
+init_main_routes(app)
+init_posts_routes(app)
 
 application = app
